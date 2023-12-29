2f7b1f102f2ffd3d74035aae3a1cbb2dfa548375
root <alanborges2020@outlook.comm>
2023-12-29 18:04:59 -0300

    Subindo Projeto

---
+++ src/br/com/streams/Pessoa.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.streams;
+
+import java.util.List;
+
+/**
+ *
+ * @author alan.menezes
+ */
+public class Pessoa {
+    
+    private String nome;
+    private String sexo;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String sexo) {
+        this.nome = nome;
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+    public List listaDePessoas(){
+        Pessoa pessoa1 = new Pessoa("Matheus Henrique", "M");
+        Pessoa pessoa2 = new Pessoa("Hernandez Roja", "M");
+        Pessoa pessoa3 = new Pessoa("Mario Fernandez", "M");
+        Pessoa pessoa4 = new Pessoa("Cinthia", "F");
+        Pessoa pessoa5 = new Pessoa("Marta", "F");
+        Pessoa pessoa6 = new Pessoa("Elisa", "F");
+        return List.of(pessoa1,pessoa2,pessoa3,pessoa4, pessoa5, pessoa6);
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" + "nome=" + nome + ", sexo=" + sexo + '}';
+    }
+    
+    
+    
+}
---
+++ src/br/com/streams/main.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.streams;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ *
+ * @author alan.menezes
+ */
+public class main {
+
+    public static void main(String[] args) {
+        listadepessoas();
+        lerListaSomenteMulher();
+        listaPessoasSeparadosPorVirgulasLambda();
+        listaSeparadosPorVirgulaPorGrupoGenero();
+    }
+
+    static void listadepessoas() {
+        System.out.println("\n" + "****lista Completa de pessoas******" + "\n");
+        List<Pessoa> lista = new Pessoa().listaDePessoas();
+        lista.stream().forEach(pessoa -> System.out.println(pessoa));
+    }
+
+    static void lerListaSomenteMulher() {
+        System.out.println("\n" + "***Somente lista de Mulheres*****" + "\n");
+        List<Pessoa> lista = new Pessoa().listaDePessoas();
+        lista.stream()
+                .filter(p -> p.getSexo().equals("F"))
+                .forEach(pessoa -> System.out.println(pessoa));
+    }
+
+        
+    static void listaPessoasSeparadosPorVirgulasLambda(){
+         System.out.println("\n" + "****** Lista de Pessoas Separado por virgula usando expresão Lambda*****" + "\n");
+         List<Pessoa> lista = new Pessoa().listaDePessoas();
+          String nomePessoaSexo = ",";
+          String[] nomes = nomePessoaSexo.split(" ; ");
+          lista.stream().forEach(pp->System.out.println(pp + nomes[0]));             
+    }
+    
+    private static void imprimirGrupo(Map<String, List<Pessoa>> gruposPorGenero, String genero) {
+    List<Pessoa> pessoasDoGenero = gruposPorGenero.getOrDefault(genero, Collections.emptyList());
+
+    if (!pessoasDoGenero.isEmpty()) {
+        String resultado = pessoasDoGenero.stream()
+                .map(Pessoa::toString)
+                .collect(Collectors.joining(", "));
+        System.out.println(resultado);
+    } else {
+        System.out.println("Nenhum registro para o grupo " + genero);
+    }
+}
+    
+    static void listaSeparadosPorVirgulaPorGrupoGenero(){
+        System.out.println("\n" + "****** Lista de Pessoas Separado por Genero*****" + "\n");
+        List<Pessoa> lista = new Pessoa().listaDePessoas();
+
+        Map<String, List<Pessoa>> gruposPorGenero = lista.stream()
+                .collect(Collectors.groupingBy(Pessoa::getSexo));
+
+        System.out.println("******************* Grupo Masculino ********************");
+        imprimirGrupo(gruposPorGenero, "M");
+
+        System.out.println("******************* Grupo Feminino ********************");
+        imprimirGrupo(gruposPorGenero, "F");
+    }
+}
